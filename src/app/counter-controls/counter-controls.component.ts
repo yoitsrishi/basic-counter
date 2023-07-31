e5bf81bcfcba1bccd2ba36ba77c56f73f0fa758f
Rishi Ranjan <mrcodinggeek@gmail.com>
2023-07-31 18:50:10 +0530

    Handling state without inbuilt function in ngrx

--- src/app/counter-controls/counter-controls.component.ts
+++ src/app/counter-controls/counter-controls.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 import { CounterService } from '../counter.service';
 import { Store } from '@ngrx/store';
-import { increment } from '../store/counter.actions';
+import { IncrementAction } from '../store/counter.actions';
+// import { increment } from '../store/counter.actions';
 
 @Component({
   selector: 'app-counter-controls',
@@ -14,7 +15,7 @@ export class CounterControlsComponent {
   constructor(private store: Store) {}
 
   increment() {
-    this.store.dispatch(increment({ value: 2 }));
+    this.store.dispatch(new IncrementAction(2));
   }
 
   decrement() {}
--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,12 +1,18 @@
-import { createReducer, on } from '@ngrx/store';
-import { increment } from './counter.actions';
+import { Action, createReducer, on } from '@ngrx/store';
+import { CounterActions, IncrementAction } from './counter.actions';
 
 const initialState = 0;
-export const counterReducer = createReducer(
-  initialState,
-  on(increment, (state, action) => state + action.value)
-);
+// export const counterReducer = createReducer(
+//   initialState,
+//   on(increment, (state, action) => state + action.value)
+// );
 
-// export function counterReducer(state=initialState){
-//     return state
-// } alternative way of writing reducer
+export function counterReducer(
+  state = initialState,
+  action: CounterActions | Action
+) {
+  if (action.type === '[Counter] Increment') {
+    return state + (action as IncrementAction).value;
+  }
+  return state;
+}
